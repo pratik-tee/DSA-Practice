fa9c17bb41e1c005fcaa96412ae0503036b5a21f
Pratik <156935609+pratik-tee@users.noreply.github.com>
2026-08-20 01:27:50 +0530

    Time: 15 ms (17.81%), Space: 11.1 MB (19.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n=s.size(),maxl=0,l=0,r=0,maxf=0;
-         unordered_map<char,int>map;
-         while(r<n){
-            map[s[r]]++;
-            maxf=max(maxf,map[s[r]]);
-             if(r-l+1-maxf>k){
-               map[s[l]]--;
-               l++;
-            }
-           if(r-l+1-maxf<=k )maxl=max(maxl,r-l+1);
-            r++;
-         }
-         
-         return maxl;
+        int n=s.size();
+        unordered_map<int,int>mp;
+        int l=0,maxi=0,maxl=0;
+        for(int r=0;r<n;r++){
+             mp[s[r]]++;
+             maxi=max(maxi,mp[s[r]]);
+             if(r-l+1-maxi>k){
+                  mp[s[l]]--;
+                  l++;
+             }
+             if(r-l+1-maxi<=k) maxl=max(maxl,r-l+1);
+        }
+        return maxl;
     }
 };
